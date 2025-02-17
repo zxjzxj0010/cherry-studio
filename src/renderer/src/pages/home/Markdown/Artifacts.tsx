87fa2cdf1ee031ae2fd5a1c863bf64f9a4b51650
eeee0717 <chentao020717Work@outlook.com>
2025-02-17 16:53:21 +0800

    feat: artifacts add open external

--- src/renderer/src/pages/home/Markdown/Artifacts.tsx
+++ src/renderer/src/pages/home/Markdown/Artifacts.tsx
@@ -1,9 +1,9 @@
-import { DownloadOutlined, ExpandOutlined } from '@ant-design/icons'
+import { CloseOutlined, DownloadOutlined, ExpandOutlined } from '@ant-design/icons'
 import MinApp from '@renderer/components/MinApp'
 import { AppLogo } from '@renderer/config/env'
 import { extractTitle } from '@renderer/utils/formats'
-import { Button } from 'antd'
-import { FC } from 'react'
+import { Button, Modal } from 'antd'
+import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
@@ -14,16 +14,42 @@ interface Props {
 const Artifacts: FC<Props> = ({ html }) => {
   const { t } = useTranslation()
   const title = extractTitle(html) || 'Artifacts' + ' ' + t('chat.artifacts.button.preview')
+  const [previewModalVisible, setPreviewModalVisible] = useState(false)
 
   const onPreview = async () => {
+    setPreviewModalVisible(true) // 显示 Modal
+  }
+
+  const handlePreviewModalOk = async () => {
+    setPreviewModalVisible(false)
     const path = await window.api.file.create('artifacts-preview.html')
     await window.api.file.write(path, html)
-
+    const filePath = `file://${path}`
     MinApp.start({
       name: title,
       logo: AppLogo,
-      url: `file://${path}`
+      url: filePath
     })
+  }
+
+  const handlePreviewModalCancel = async () => {
+    setPreviewModalVisible(false)
+    const path = await window.api.file.create('artifacts-preview.html')
+    await window.api.file.write(path, html)
+    const filePath = `file://${path}`
+
+    if (window.api.shell && window.api.shell.openExternal) {
+      window.api.shell.openExternal(filePath)
+    } else {
+      Modal.error({
+        title: t('chat.artifacts.preview.modal.error.title'),
+        content: t('chat.artifacts.preview.modal.error.content')
+      })
+    }
+  }
+
+  const handlePreviewModalClose = () => {
+    setPreviewModalVisible(false)
   }
 
   const onDownload = () => {
@@ -38,6 +64,26 @@ const Artifacts: FC<Props> = ({ html }) => {
       <Button icon={<DownloadOutlined />} onClick={onDownload} size="small">
         {t('chat.artifacts.button.download')}
       </Button>
+
+      <Modal
+        title={t('chat.artifacts.preview.modal.title')}
+        open={previewModalVisible}
+        onOk={handlePreviewModalOk}
+        onCancel={handlePreviewModalClose}
+        okText={t('chat.artifacts.preview.modal.okText')}
+        cancelText={t('chat.artifacts.preview.modal.cancelText')}
+        closable={true}
+        closeIcon={<CloseOutlined />}
+        footer={[
+          <Button key="cancel" onClick={handlePreviewModalCancel}>
+            {t('chat.artifacts.preview.modal.cancelText')}
+          </Button>,
+          <Button key="submit" type="primary" onClick={handlePreviewModalOk}>
+            {t('chat.artifacts.preview.modal.okText')}
+          </Button>
+        ]}>
+        <p>{t('chat.artifacts.preview.modal.content')}</p>
+      </Modal>
     </Container>
   )
 }
