9971809b4f6d1367db685383031693d4b2567a64
eeee0717 <chentao020717Work@outlook.com>
2025-02-18 10:05:00 +0800

    fix: remove modal

--- src/renderer/src/pages/home/Markdown/Artifacts.tsx
+++ src/renderer/src/pages/home/Markdown/Artifacts.tsx
@@ -1,9 +1,9 @@
-import { CloseOutlined, DownloadOutlined, ExpandOutlined } from '@ant-design/icons'
+import { DownloadOutlined, ExpandOutlined, LinkOutlined } from '@ant-design/icons'
 import MinApp from '@renderer/components/MinApp'
 import { AppLogo } from '@renderer/config/env'
 import { extractTitle } from '@renderer/utils/formats'
-import { Button, Modal } from 'antd'
-import { FC, useState } from 'react'
+import { Button } from 'antd'
+import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
@@ -13,15 +13,12 @@ interface Props {
 
 const Artifacts: FC<Props> = ({ html }) => {
   const { t } = useTranslation()
-  const title = extractTitle(html) || 'Artifacts' + ' ' + t('chat.artifacts.button.preview')
-  const [previewModalVisible, setPreviewModalVisible] = useState(false)
+  const title = extractTitle(html) || 'Artifacts ' + t('chat.artifacts.button.preview')
 
-  const onPreview = async () => {
-    setPreviewModalVisible(true) // 显示 Modal
-  }
-
-  const handlePreviewModalOk = async () => {
-    setPreviewModalVisible(false)
+  /**
+   * 在应用内打开
+   */
+  const handleOpenInApp = async () => {
     const path = await window.api.file.create('artifacts-preview.html')
     await window.api.file.write(path, html)
     const filePath = `file://${path}`
@@ -32,8 +29,10 @@ const Artifacts: FC<Props> = ({ html }) => {
     })
   }
 
-  const handlePreviewModalCancel = async () => {
-    setPreviewModalVisible(false)
+  /**
+   * 外部链接打开
+   */
+  const handleOpenExternal = async () => {
     const path = await window.api.file.create('artifacts-preview.html')
     await window.api.file.write(path, html)
     const filePath = `file://${path}`
@@ -41,49 +40,30 @@ const Artifacts: FC<Props> = ({ html }) => {
     if (window.api.shell && window.api.shell.openExternal) {
       window.api.shell.openExternal(filePath)
     } else {
-      Modal.error({
-        title: t('chat.artifacts.preview.modal.error.title'),
-        content: t('chat.artifacts.preview.modal.error.content')
-      })
+      console.error(t('artifacts.preview.openExternal.error.content'))
     }
   }
 
-  const handlePreviewModalClose = () => {
-    setPreviewModalVisible(false)
-  }
-
+  /**
+   * 下载文件
+   */
   const onDownload = () => {
     window.api.file.save(`${title}.html`, html)
   }
 
   return (
     <Container>
-      <Button type="primary" icon={<ExpandOutlined />} onClick={onPreview} size="small">
+      <Button icon={<ExpandOutlined />} onClick={handleOpenInApp}>
         {t('chat.artifacts.button.preview')}
       </Button>
-      <Button icon={<DownloadOutlined />} onClick={onDownload} size="small">
-        {t('chat.artifacts.button.download')}
+
+      <Button icon={<LinkOutlined />} onClick={handleOpenExternal}>
+        {t('chat.artifacts.button.openExternal')}
       </Button>
 
-      <Modal
-        title={t('chat.artifacts.preview.modal.title')}
-        open={previewModalVisible}
-        onOk={handlePreviewModalOk}
-        onCancel={handlePreviewModalClose}
-        okText={t('chat.artifacts.preview.modal.okText')}
-        cancelText={t('chat.artifacts.preview.modal.cancelText')}
-        closable={true}
-        closeIcon={<CloseOutlined />}
-        footer={[
-          <Button key="cancel" onClick={handlePreviewModalCancel}>
-            {t('chat.artifacts.preview.modal.cancelText')}
-          </Button>,
-          <Button key="submit" type="primary" onClick={handlePreviewModalOk}>
-            {t('chat.artifacts.preview.modal.okText')}
-          </Button>
-        ]}>
-        <p>{t('chat.artifacts.preview.modal.content')}</p>
-      </Modal>
+      <Button icon={<DownloadOutlined />} onClick={onDownload}>
+        {t('chat.artifacts.button.download')}
+      </Button>
     </Container>
   )
 }
